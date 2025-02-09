b687eb5fd77f510b71f78b55ad1c0d3805f129c6
Rukundo-Bahati <rukundorca@gmail.com>
2025-02-09 17:55:24 +0200

    handling connect error

--- db/connection.js
+++ db/connection.js
@@ -6,6 +6,9 @@ export default mongoose
   .connect(CONNECTION,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
     connectTimeoutMS: 30000,
   })
   .then(() => console.log(`Connected Successfully Umurava DB`))
